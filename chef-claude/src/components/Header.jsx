6215985080e931186942cf2d156e738dfae08944
Halil YILMAZ <halil.yilmaz@ayoff.com.tr>
2025-01-28 14:04:03 +0300

    Header component added

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <img className="img-chef" src="/images/chef.webp" alt="chef" />
+            <h1 className="title">Chef Claude</h1>
+        </header>
+    )
+}
